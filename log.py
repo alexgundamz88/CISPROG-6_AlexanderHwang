df1e4a85a4d9fb993c04c25789b6b0d1787bc2e2
alexgundamz88 <37199460+alexgundamz88@users.noreply.github.com>
2021-11-06 20:26:53 -0700

    Add files via upload

---
+++ log.py
@@ -0,0 +1,12 @@
+   1: # print("I'm a code cell")
+   2: print("Hi")
+   3: ?
+   4:
+import collections
+collections.namedtuple?
+   5: collections.Counter??
+   6: *int*?
+   7: %quickref
+   8: collections.<TAB>
+   9: 2+10
+  10: _+10
